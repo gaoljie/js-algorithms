f910a8542e4fc28e3a89600c458d6811e2af5f2a
Jay Gao <gaoljie@gmail.com>
2019-07-01 10:24:07 +0800

    cartesian

---
+++ algorithms/03 - Sets/01 - Cartesian Product/index.js
@@ -0,0 +1,17 @@
+function cartesian(setA, setB) {
+  if (!setA || !setB || !setA.length || !setB.length) {
+    return null
+  }
+
+  let products = []
+
+  setA.forEach(itemA => {
+    setB.forEach(itemB => {
+      products.push([itemA, itemB])
+    })
+  })
+
+  return products
+}
+
+console.log(cartesian([1, 2], [3, 5]))
