7167371ede91980c06e8c31ba584c49f958cef64
Jay Gao <gaoljie@gmail.com>
2019-07-06 17:09:27 +0800

    scs

--- algorithms/03 - Sets/08 - Shortest Common Supersequence/index.js
+++ algorithms/03 - Sets/08 - Shortest Common Supersequence/index.js
@@ -1,15 +1,3 @@
-function lcsWithString(a, b) {
-  if (!b || !a) return ''
-  if (a[0] === b[0]) {
-    return a[0] + lcsWithString(a.slice(1), b.slice(1))
-  }
-
-  let strA = lcsWithString(a, b.slice(1))
-  let strB = lcsWithString(a.slice(1), b)
-
-  return (strA.length > strB.length) ? strA : strB
-}
-
 function scs(a, b) {
   if (!a) return b
   if (!b) return a
