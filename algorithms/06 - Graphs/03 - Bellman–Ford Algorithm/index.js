688cd717897b239a48dcab6ea272133d49588531
Jay Gao <gaoljie@gmail.com>
2019-07-16 20:43:33 +0800

    bellford

---
+++ algorithms/06 - Graphs/03 - Bellman–Ford Algorithm/index.js
@@ -0,0 +1,45 @@
+function bellmanFord(graph, source) {
+  let dist = {}
+
+  for (let edge of graph) {
+    let vertex1 = edge[0]
+    let vertex2 = edge[1]
+
+    dist[vertex1] = Infinity;
+    dist[vertex2] = Infinity;
+  }
+
+  dist[source] = 0
+
+  for (let vertex of Object.keys(dist)) {
+    let changed = false
+
+    for (let edge of graph) {
+      let start = edge[0]
+      let end = edge[1]
+      let len = edge[2]
+
+      if (dist[start] + len < dist[end]) {
+        dist[end] = dist[start] + len
+        changed = true
+      }
+    }
+
+    if (!changed) {
+      break
+    }
+  }
+
+  return dist
+}
+
+console.log(bellmanFord([
+  ['S', 'E', 8],
+  ['S', 'A', 10],
+  ['E', 'D', 1],
+  ['D', 'A', -4],
+  ['D', 'C', -1],
+  ['A', 'C', 2],
+  ['C', 'B', -2],
+  ['B', 'A', 1]
+], 'S'))
